d1f698d85b5ccb013e8411874e06e89d81968b88
b05902062 <b05902062@ntu.edu.tw>
2019-10-19 11:47:18 +0800

    add g_video_sample variable

--- encoder.py
+++ encoder.py
@@ -1,12 +1,10 @@
 import torchvision.models as models
 import torch.nn as nn
 import torch
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
 g_encode_dim=512
 g_encode_kernel=3#odd
-
+g_video_sample=25
 
 class frame_feature_extraction(nn.Module):
 	def __init__(self,model,transfer_dim):
@@ -27,11 +25,11 @@ class video_feature_extraction(nn.Module):
 		self.encode_dim=encode_dim
 		self.frame_encode=frame_feature_extraction("resnet18",encode_dim)
 	def forward(self,x):
-		#x is a 5d tensor of size batch*25*channel*height*width
+		#x is a 5d tensor of size batch*g_video_sample*channel*height*width
 		x=x.reshape(-1,x.shape[2],x.shape[3],x.shape[4])
 		x=self.frame_encode(x)
-		x=x.reshape(-1,25,self.encode_dim)
-		#return batch*25*encode_dim
+		x=x.reshape(-1,g_video_sample,self.encode_dim)
+		#return batch*g_video_sample*encode_dim
 		return x
 
 class TDconv(nn.Module):
@@ -43,34 +41,34 @@ class TDconv(nn.Module):
 
 
 	def forword(self,x):
-		#input x is of size batch*25*encode_dim
+		#input x is of size batch*g_video_sample*encode_dim
 		x_filter=x.clone()
 		x_temp=x.clone()
 
-		x_filter=x_filter.reshape(x_filter.shape[0],1,-1)#return batch*1*(encode_dim*25) 3d tensor.
-		x_filter=self.deform_conv(x_filter)#return batch*g_encode_kernel*25
+		x_filter=x_filter.reshape(x_filter.shape[0],1,-1)#return batch*1*(encode_dim*g_video_sample) 3d tensor.
+		x_filter=self.deform_conv(x_filter)#return batch*g_encode_kernel*g_video_sample
 		x_filter=x_filter.transpose(1,2)
-		#x_filter is of size batch*25*g_encode_kernel now
+		#x_filter is of size batch*g_video_sample*g_encode_kernel now
 		
-		x_filter=x_filter.reshape(x_filter.shape[0],g_encode_kernel*25,1)).ex_filterpand(-1,-1,25)
-		a=torch.tensor([o for o in range(i-g_encode_kernel,i+g_encode_kernel+1 for i in range(0,25)]).reshape(1,-1,1).ex_filterpand(x_filter.shape).to_device()
-		b=torch.from_numpy(np.arange(25)).reshape(1,1,-1).ex_filterpand(x_filter.shape).to_device()
-		#x_filter, a, and b are now all of size batch*(g_encode_kernel*25)*25 3d tensor.
+		x_filter=x_filter.reshape(x_filter.shape[0],g_encode_kernel*g_video_sample,1)).expand(-1,-1,g_video_sample)
+		a=torch.tensor([o for o in range(i-g_encode_kernel,i+g_encode_kernel+1 for i in range(0,g_video_sample)]).reshape(1,-1,1).expand(x_filter.shape).to_device()
+		b=torch.from_numpy(np.arange(g_video_sample)).reshape(1,1,-1).expand(x_filter.shape).to_device()
+		#x_filter, a, and b are now all of size batch*(g_encode_kernel*g_video_sample)*g_video_sample 3d tensor.
 		x_filter=x_filter+a-b
 		x_filter=1-torch.absolute(x_filter)
 		x_filter=nn.relu(x_filter)
 		
-		x=x.unsqueeze(dim=1)#return batch*1*25*encode_dim
-		x_filter=x_filter.unsqueeze(dim=3)#return  batch*(g_encode_kernel*25)*25*1
+		x=x.unsqueeze(dim=1)#return batch*1*g_video_sample*encode_dim
+		x_filter=x_filter.unsqueeze(dim=3)#return  batch*(g_encode_kernel*g_video_sample)*g_video_sample*1
 		x=(x*x_filter).sum(dim=2)#member-wise multiplication broadcast
-		#x is now batch*(g_encode_kernel*25)*encode_dim
+		#x is now batch*(g_encode_kernel*g_video_sample)*encode_dim
 		x=x.reshape(x.shape[0],1,-1)
-		#x is now batch*(g_encode_kernel*25*encode_dim)
+		#x is now batch*(encode_dim*g_encode_kernel*g_video_sample)
 		x=self.attention_conv(x)
-		#x is now batch*(2*encode_dim)*25 3d
+		#x is now batch*(2*encode_dim)*g_video_sample 3d
 		x=x.transpose(1,2)
 		x=sig_gate(x)
-		#x is now batch*25*encode_dim
+		#x is now batch*g_video_sample*encode_dim
 		x=x+x_temp
 
 		return x
@@ -88,10 +86,10 @@ class TDconvE(nn.Module):
 		self.TDenc1=TDconv(encoder_dim)
 		self.TDenc2=TDconv(encoder_dim)
 	def forward(self,x):
-		#video of size batch*25*channel*height*width
-		x=self.video_encoder(x)#return batch*25*encode_dim
-		x=self.TDenc1(x)#return batch*25*encode_dim
-		x=self.TDenc2(x)#return batch*25*encode_dim
+		#video of size batch*g_video_sample*channel*height*width
+		x=self.video_encoder(x)#return batch*g_video_sample*encode_dim
+		x=self.TDenc1(x)#return batch*g_video_sample*encode_dim
+		x=self.TDenc2(x)#return batch*g_video_sample*encode_dim
 		return x	
 
 
@@ -100,6 +98,6 @@ class TDconvE(nn.Module):
 if __name__=='__main__':
 	
 	encoder=TDconvE(g_encode_dim)
-	#video of size batch*25*channel*height*width
-	encoding=encoder(video)#return batch*25*g_encode_dim
+	#video of size batch*g_video_sample*channel*height*width
+	encoding=encoder(video)#return batch*g_video_sample*g_encode_dim
 	
