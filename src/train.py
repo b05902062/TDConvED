594e864e5deb481b9df5b3b46b5c7a3e7bd6b629
b05902062 <b05902062@meow1.csie.ntu.edu.tw>
2019-11-16 01:12:09 +0800

    runnable but loss is not dropping

--- src/train.py
+++ src/train.py
@@ -5,11 +5,12 @@ import torch
 import torch.nn as nn
 import argparse
 from torch.utils.data import DataLoader
+import uuid
 
-output=open("./log","w+") 
 
 def train(args):
 	
+	output=open("./"+"_".join([str(args.lr),str(args.encoder_dim),str(args.decoder_dim),str(args.embed_dim)])+f"_{uuid.uuid4()}.txt","w+") 
 	device = torch.device(args.device if torch.cuda.is_available() else 'cpu')
 
 	msr_vtt=msr_vtt_dataset(args.data_dir,"train",args.batch_size)
@@ -19,12 +20,12 @@ def train(args):
 	decoder = TDconvD(args.embed_dim, args.decoder_dim, args.encoder_dim, len(msr_vtt.w2i)).to(device)
 
 	criterion = nn.CrossEntropyLoss(ignore_index=0)
-	params = list(decoder.parameters()) + list(encoder.parameters())
+	params = list(decoder.shifted_conv1.parameters())+list(decoder.shifted_conv2.parameters())+list(decoder.linear2.parameters())+list(decoder.linear1.parameters()) + list(encoder.parameters())
 	optimizer = torch.optim.Adam(params, lr=args.lr)
 
 	for i_epoch in range(args.epoch):
 		for i_b,(images,sen_in,lengths) in enumerate(data):
-			if i_b%500==499:
+			if i_b%500==50:
 				break
 			
 			images=images.squeeze(0).to(device)
@@ -34,11 +35,13 @@ def train(args):
 			#len is a list(len=batch) of int(length of each sentence in this batch including <sos> and <eos>).
 
 			features=encoder(images)	
+			#feature is of size batch*g_sample*encoder_dim.
 			outputs=decoder(features,sen_in,lengths)
+			#feature is of size batch*(max_label_len-1)*vocab_size
 			loss=criterion(outputs.reshape(-1,outputs.shape[2]),sen_in[:,1:].reshape(-1))
 			if i_b%1 ==0:
-				print(f"Epoch: {i_epoch+1:05}/{args.epoch} , Batch: {i_b+1}/{len(data)} Loss: {loss}")
-				output.write(f"Epoch: {i_epoch+1:05}/{args.epoch} , Batch: {i_b+1}/{len(data)} Loss: {loss}")
+				print(f"Epoch: {i_epoch+1}/{args.epoch} , Batch: {i_b+1}/{len(data)} Loss: {loss}")
+				output.write(f"Epoch: {i_epoch+1}/{args.epoch} , Batch: {i_b+1}/{len(data)} Loss: {loss}\n")
 			encoder.zero_grad()
 			decoder.zero_grad()
 			loss.backward()
@@ -53,11 +56,11 @@ if __name__=="__main__":
 	parser = argparse.ArgumentParser(description='train TDconvED')
 	parser.add_argument('--data_dir',default='./data',help='directory for sampled images')
 	parser.add_argument('--batch_size',type=int,default=16,help='batch size')
-	parser.add_argument('--encoder_dim',type=int,default=256,help='dimension for TDconvE')
-	parser.add_argument('--decoder_dim',type=int,default=256,help='dimension for TDconvD')
-	parser.add_argument('--embed_dim',type=int,default=256,help='dimension for word embedding')
+	parser.add_argument('--encoder_dim',type=int,default=32,help='dimension for TDconvE')
+	parser.add_argument('--decoder_dim',type=int,default=32,help='dimension for TDconvD')
+	parser.add_argument('--embed_dim',type=int,default=32,help='dimension for word embedding')
 	parser.add_argument('--device',type=str,default='cuda:0',help='default to cuda:0 if gpu available else cpu')
-	parser.add_argument('--epoch',type=int,default=10,help='total epochs to train.')
+	parser.add_argument('--epoch',type=int,default=100,help='total epochs to train.')
 	parser.add_argument('--lr',type=float,default=0.001,help='learning rate for optimizer.')
 	#parser.add_argument('--attention_size',type=int,default=256,help='dimension for attention')
 
